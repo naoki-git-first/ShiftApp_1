ae12b32c12679c5d15eaba8f1f4014d1a77b77e8
Shun Ueji <136816807+chinsukou@users.noreply.github.com>
2023-11-21 15:01:35 +0900

    メニューボタン追加

--- .history/src/app/index_20231121145703.tsx
+++ .history/src/app/index_20231121145703.tsx
@@ -40,31 +40,27 @@ const styles = StyleSheet.create({
     flex: 1
   },
   checkShift: {
-    flex: 1,
     backgroundColor: '#ff0000',
     fontSize: 34,
-    paddingVertical: 24,
+    paddingVertical: 4,
     paddingHorizontal: 8
   },
   submitShift: {
-    flex: 1,
     backgroundColor: '#00ff00',
     fontSize: 34,
-    paddingVertical: 24,
+    paddingVertical: 4,
     paddingHorizontal: 8
   },
   editShift: {
-    flex: 1,
     backgroundColor: '#0000ff',
     fontSize: 34,
-    paddingVertical: 24,
+    paddingVertical: 4,
     paddingHorizontal: 8
   },
   other: {
-    flex: 1,
     backgroundColor: '#ffff00',
     fontSize: 34,
-    paddingVertical: 24,
+    paddingVertical: 4,
     paddingHorizontal: 8
   }
 })
