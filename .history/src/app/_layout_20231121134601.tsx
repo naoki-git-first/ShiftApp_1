77e60b498e36a82d18ad27dc12ec28a2e8e47bee
Shun Ueji <136816807+chinsukou@users.noreply.github.com>
2023-11-23 15:59:02 +0900

    四角ボタンのコンポーネント化

--- .history/src/app/_layout_20231121134601.tsx
+++
@@ -1,7 +0,0 @@
-import { Slot } from 'expo-router'
-
-const Layout = (): JSX.Element => {
-  return <Slot />
-}
-
-export default Layout
--- .history/src/app/shift/submit_form_20231122194935.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from 'react-native'
-
-const SubmitForm = (): JSX.Element => {
-  return (
-    <View></View>
-  )
-}
-
-export default SubmitForm
