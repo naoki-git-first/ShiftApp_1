1ba089683edac3e3c4580b6f57ab82165d69bbab
Shun Ueji <136816807+chinsukou@users.noreply.github.com>
2023-12-14 01:53:26 +0900

    店舗詳細画面への遷移とその店舗の詳細情報取得できた

--- src/app/utility/shop_info.tsx
+++ src/app/utility/shop_info.tsx
@@ -1,30 +1,60 @@
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
-import { router } from 'expo-router'
+import { router, useLocalSearchParams } from 'expo-router'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import CircleButton from '../../components/CircleButton'
+import { useEffect, useState } from 'react'
+import { type Shop } from '../types/shop'
+import { auth, db } from '../../config'
+import { doc, onSnapshot } from 'firebase/firestore'
 
 const handlePress = (): void => {
   router.push('utility/edit_shop')
 }
 
 const ShopInfo = (): JSX.Element => {
+  const id = String(useLocalSearchParams().id)
+  const [shop, setShop] = useState<Shop | null>(null)
+  useEffect(() => {
+    if (auth.currentUser === null) { return }
+    const ref = doc(db, `users/${auth.currentUser.uid}/shops`, id)
+    const unsubscribe = onSnapshot(ref, (shopDoc) => {
+      const {
+        shopName,
+        shopManager,
+        address,
+        businessDay,
+        regularClosingDay,
+        updatedAt
+      } = shopDoc.data() as Shop
+      setShop({
+        id: shopDoc.id,
+        shopName,
+        shopManager,
+        address,
+        businessDay,
+        regularClosingDay,
+        updatedAt
+      })
+    })
+    return unsubscribe
+  }, [])
   return (
     <SafeAreaView style={styles.safeArea}>
       <View>
-        <Text style={styles.text}>店舗名：</Text>
+        <Text style={styles.text}>店舗名：{shop?.shopName}</Text>
       </View>
       <View>
-        <Text style={styles.text}>店長：</Text>
+        <Text style={styles.text}>店長：{shop?.shopManager}</Text>
       </View>
       <View>
-        <Text style={styles.text}>住所：</Text>
+        <Text style={styles.text}>住所：{shop?.address}</Text>
       </View>
       <View>
-        <Text style={styles.text}>営業日：</Text>
+        <Text style={styles.text}>営業日：{shop?.businessDay}</Text>
       </View>
       <View>
-        <Text style={styles.text}>定休日：</Text>
+        <Text style={styles.text}>定休日：{shop?.regularClosingDay}</Text>
       </View>
       <CircleButton buttonColor='#22ff22' textColor='white' onPress={handlePress}>
         <MaterialIcons name='edit' size={40} />
--- src/components/ShopList.tsx
+++ src/components/ShopList.tsx
@@ -1,7 +1,7 @@
 import { Text, TouchableOpacity, StyleSheet } from 'react-native'
 import { type Shop } from '../app/types/shop'
 
-import { auth, db } from '../config'
+import { Link } from 'expo-router'
 
 interface Props {
   onPress?: () => void
@@ -13,9 +13,14 @@ const List = (props: Props): JSX.Element | null => {
   const { shopName } = shop
   if (shopName === null) { return null }
   return (
-    <TouchableOpacity style={styles.listContainer} onPress={onPress}>
-      <Text style={styles.text}>{shopName}</Text>
-    </TouchableOpacity>
+    <Link
+    href={{ pathname: 'utility/shop_info', params: { id: shop.id } }}
+    asChild
+  >
+      <TouchableOpacity style={styles.listContainer} onPress={onPress}>
+        <Text style={styles.text}>{shopName}店</Text>
+      </TouchableOpacity>
+    </Link>
   )
 }
 
